697d7451e8902d31bb361d1cba57de9b25bbceef
Ondřej Ezr <oezr@msp.justice.cz>
2018-06-23 12:21:35 +0200

    repair tests, deploy to lab

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,13 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module DocumentGenerator
+
+  autoload :VariablesCoder, 'document_generator/variables_coder'
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
 end
