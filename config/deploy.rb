f80f32c92418ea3444af5587333bd297904c9992
Ondřej Ezr <oezr@msp.justice.cz>
2018-06-25 01:59:15 +0200

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
-lock "~> 3.11"
+lock "~> 3.10"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "document_generator"
+set :repo_url, "git@git.servis.justice.cz:justice_core/document_generator.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/srv/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,10 +21,11 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/unicorn.rb", "config/config.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,3 +35,12 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+# after "deploy:symlink:linked_dirs", "deploy:symlink_config_files"
+
+set :bundle_env_variables, {
+  'http_proxy' => 'http://proxy.justice.cz:3128/',
+  'https_proxy' => 'http://proxy.justice.cz:3128/'
+}
+
+set :sidekiq_options, "-q default -q mailers"
