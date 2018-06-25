23f8e66fb12efe8bd4569f6d18425db58a79bcf7
Ondřej Ezr <oezr@msp.justice.cz>
2018-06-25 02:40:58 +0200

    initializers

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-module DocumentGenerator
-  TEMPLATES_DIR = Rails.root.join('files')
-  FORMATS = [:html, :markdown]
-end
---
+++ config/initializers/document_generator.rb
@@ -0,0 +1,4 @@
+module DocumentGenerator
+  TEMPLATES_DIR = Rails.root.join('files')
+  FORMATS = [:html, :markdown]
+end
